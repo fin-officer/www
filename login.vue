1d29b3fdeb5f25bf96bdaff86e677fa208e5632d
Tom Sapletta <tom-sapletta-com@users.noreply.github.com>
2023-09-18 16:44:54 +0200

    Create login.vue

---
+++ login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <input v-model="username" placeholder="Username" />
+    <input v-model="password" type="password" placeholder="Password" />
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      axios.post('/api/login', {
+        username: this.username,
+        password: this.password
+      }).then(response => {
+        console.log(response);
+      }).catch(error => {
+        console.error(error);
+      });
+    }
+  }
+}
+</script>
