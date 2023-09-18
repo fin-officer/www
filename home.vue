fbef82087c8c9f4b42d8546a3e34999511b74b65
Tom Sapletta <tom-sapletta-com@users.noreply.github.com>
2023-09-18 16:44:43 +0200

    Create home.vue

---
+++ home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Special Offer</h1>
+    <p>Join our newsletter to stay updated!</p>
+    <input v-model="email" placeholder="Your email" />
+    <button @click="subscribe">Subscribe</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      email: ''
+    }
+  },
+  methods: {
+    subscribe() {
+      axios.post('/api/subscribe', {
+        email: this.email
+      }).then(response => {
+        console.log(response);
+      }).catch(error => {
+        console.error(error);
+      });
+    }
+  }
+}
+</script>
